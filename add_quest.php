5b610668e5ce0cad59a8ce7c31c48f12d79bdfe8
KIvanovs <ekspanser@gmail.com>
2023-04-24 13:26:07 +0300

    add quest now can add photo

--- add_quest.php
+++ add_quest.php
@@ -11,30 +11,55 @@ if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get form data
-$name = mysqli_real_escape_string($conn, $_POST['name']);// <-- this is ti deny syntax error
-$adress = $_POST['adress'];
-$discount = $_POST['discount'];
-$peopleAmount = $_POST['peopleAmount'];
-$ageLimit = $_POST['ageLimit'];
-$description = $_POST['description'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// Get form data
+	$name = mysqli_real_escape_string($conn, $_POST['name']);
+	$adress = $_POST['adress'];
+	$category = $_POST['category'];
+	$discount = $_POST['discount'];
+	$peopleAmount = $_POST['peopleAmount'];
+	$ageLimit = $_POST['ageLimit'];
+	$description = $_POST['description'];
 
-// Check for duplicate name
-$query = "SELECT * FROM quests WHERE name='$name'";
-$result = mysqli_query($conn, $query);
+	// Check for duplicate name
+	$query = "SELECT * FROM quests WHERE name='$name'";
+	$result = mysqli_query($conn, $query);
 
-if (mysqli_num_rows($result) > 0) {
-	echo "Quest with this name already exists!";
-	exit;
-}
+	if (mysqli_num_rows($result) > 0) {
+		echo "Quest with this name already exists!";
+		exit;
+	}
 
-// Insert data into database
-$sql = "INSERT INTO quests (name, adress, discount, peopleAmount, ageLimit, description) VALUES ('$name', '$adress', '$discount' ,'$peopleAmount','$ageLimit','$description')";
+	// Process uploaded file
+	if (isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+		$file = $_FILES['photo'];
+		$filename = $file['name'];
+		$filetmp = $file['tmp_name'];
+		$filetype = $file['type'];
+		$filesize = $file['size'];
+		$fileerror = $file['error'];
 
-if (mysqli_query($conn, $sql)) {
-	echo "New quest added successfully!";
-} else {
-	echo "Error: " . mysqli_error($conn);
+		// Проверяем, что файл является изображением
+		$allowed_types = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
+		if (!in_array($filetype, $allowed_types)) {
+			die('Файл не является изображением');
+		}
+
+		// Сохраняем файл в папку с картинками
+		$upload_dir = 'images/';
+		$upload_path = $upload_dir . $filename;
+		move_uploaded_file($filetmp, $upload_path);
+	} else {
+		die('Ошибка при загрузке файла');
+	}
+		// Insert data into database with photo path
+		$sql = "INSERT INTO quests (name,category, adress, discount, peopleAmount, ageLimit, description, photoPath) VALUES ('$name','$category', '$adress', '$discount' ,'$peopleAmount','$ageLimit','$description', '$upload_path')";
+
+	if (mysqli_query($conn, $sql)) {
+		echo "New quest added successfully!";
+	} else {
+		echo "Error: " . mysqli_error($conn);
+	}
 }
 
 // Close database connection
--- quest_form.php
+++ quest_form.php
@@ -5,9 +5,12 @@
 </head>
 <body>
 	<h2>Add Quest</h2>
-	<form method="post" action="add_quest.php">
+	<form method="post" action="add_quest.php" enctype="multipart/form-data">
 		<label for="name">Name:</label>
 		<input type="text" name="name" required><br><br>
+
+		<label for="category">Category:</label>
+		<input type="text" name="category" required><br><br>
 
 		<label for="adress">Address:</label>
 		<input type="text" name="adress" required><br><br>
@@ -23,6 +26,9 @@
 
 		<label for="description">Description:</label>
 		<textarea name="description" required></textarea><br><br>
+
+		<label for="photo">Photo:</label>
+  		<input type="file" name="photo" id="photo" required>
 
 		<input type="submit" name="submit" value="Add Quest">
 	</form>
