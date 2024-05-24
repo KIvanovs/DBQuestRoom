830ad463d4dee5f181794a047e4a1de56fa6c65c
KIvanovs <ekspanser@gmail.com>
2024-05-24 19:30:43 +0300

    added design to register and login form

--- register_login/loginform.php
+++ register_login/loginform.php
@@ -1,19 +1,23 @@
 <?php
   $pageTitle = 'Login form';
   include_once '../includes/header.php';
-	?>
-	<form method="post" action="../register_login/login.php">
-		<h2>Login Form</h2>
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email" required>
-		
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" required>
-		
-		<input type="submit" name="login" value="Login">
-	</form>
-	<?php 
+?>
+<div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-md-6 col-lg-4">
+        <form method="post" action="../register_login/login.php" class="mt-5 p-4 border rounded bg-light">
+            <h2 class="mb-4 text-center">Login Form</h2>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" name="login" class="btn btn-primary btn-block">Login</button>
+        </form>
+    </div>
+</div>
+<?php 
 include_once '../includes/footer.php';
 ?>
-
-
--- register_login/registerform.php
+++ register_login/registerform.php
@@ -1,34 +1,43 @@
 <?php
-  $pageTitle = 'Registration form';
-  include_once '../includes/header.php';
-	?>
-	<div class="container">
-		<h1>Registration Form</h1>
-		<form action="../register_login/register.php" method="POST">
-			<label for="name">Nickname</label>
-			<input type="text" id="nickname" name="nickname" required>
-
-			<label for="name">Name</label>
-			<input type="text" id="name" name="name" required>
-
-			<label for="name">Surname</label>
-			<input type="text" id="surname" name="surname" required>
-			
-			<label for="email">Email</label>
-			<input type="email" id="email" name="email" required>
-
-			<label for="email">Phone number</label>
-			<input type="text" id="phone" name="phone" required>
-			
-			<label for="password">Password</label>
-			<input type="password" id="password" name="password" required>
-			
-			<label for="confirm_password">Confirm Password</label>
-			<input type="password" id="confirm_password" name="confirm_password" required>
-			
-			<input type="submit" value="Submit">
-		</form>
-	</div>
-	<?php 
+$pageTitle = 'Registration form';
+include_once '../includes/header.php';
+?>
+<div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-md-6 col-lg-4">
+        <form action="../register_login/register.php" method="POST" class="mt-5 p-4 border rounded bg-light">
+            <h2 class="mb-4 text-center">Registration Form</h2>
+            <div class="form-group">
+                <label for="nickname">Nickname</label>
+                <input type="text" class="form-control" id="nickname" name="nickname" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="surname">Surname</label>
+                <input type="text" class="form-control" id="surname" name="surname" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone number</label>
+                <input type="text" class="form-control" id="phone" name="phone" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        </form>
+    </div>
+</div>
+<?php 
 include_once '../includes/footer.php';
 ?>
